2bfbf34b6efe69d5d3df9e86c82f4c491e951ff3
sajumani <saju79shyni@gmail.com>
2014-04-28 23:01:42 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -28,9 +28,7 @@
   <!--footer--> 
 </div>
 <!--mainwrapper-->
-	<script src="<?php bloginfo('template_url'); ?>/js/jquery-1.7.1.js" type="text/javascript"></script> 
-    <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/s3Slider.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/custom.js"></script><!---All custom js function---->
+	
    <?php wp_footer(); ?>
 </body>
 </html>
